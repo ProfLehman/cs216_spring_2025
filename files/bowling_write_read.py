31f778ab4ff5d83c49d86ad0b973381699a65588
Jeff Lehman <jlehman@huntington.edu>
2025-03-31 10:13:46 -0400

    Update bowling_write_read.py

--- files/bowling_write_read.py
+++ files/bowling_write_read.py
@@ -5,6 +5,9 @@
 
 # code demos creating CSV and reading from CSV file
 # uses a dictionary to help calculate high scores
+#
+# code demonstrates "with" approach, thus file close is
+# handled automatically
 
 
 # *** step 1 ***
@@ -12,28 +15,28 @@
 
 # open file for writing
 file_name = "scores.csv"
-file = open(file_name, "w")
 
-file.write("name, score\n")
+with open(file_name, "w") as file:
 
-# get name
-name = input("Enter bowler name: ")
+    file.write("name, score\n")
 
-while name != "stop":
-    
-    # get score
-    score = int( input("Enter bowler score: ") )
-    
-    # write score to file
-    print( name, score ) #echo to screen
-    file.write( f"{name}, {score}\n" )
-    
     # get name
     name = input("Enter bowler name: ")
 
-    # end loop
+    while name != "stop":
+        
+        # get score
+        score = int( input("Enter bowler score: ") )
+        
+        # write score to file
+        print( name, score ) #echo to screen
+        file.write( f"{name}, {score}\n" )
+        
+        # get name
+        name = input("Enter bowler name: ")
 
-file.close()
+        # end loop
+
 print()
 
 # step 2 - read file
@@ -42,38 +45,40 @@ print()
 high_score = {} # dictionary
 
 # open file for reading
-file = open("scores.csv", "r")
+with open("scores.csv", "r") as file:
 
-# get headings and ignore
-line = file.readline()
-
-# read first line  ie. priming loop
-line = file.readline()
-
-# process line until empty line read
-# note: can also use while line
-while line != "":
-
-    # split line based on comma
-    data = line.split(",")
-    #print( data )
-    
-    # extract and convert data (ie. int and float must be converted)
-    name = data[0].strip()
-    score = int( data[1].strip() )
-    print( f"{name}, {score}" )
-    
-    if name in high_score:
-        # have seen name, so get score and update if higher
-        current_score = high_score[ name ]
-        if score > current_score:
-            high_score[ name ] = score 
-    else:
-        high_score[ name ] = score  # first time see name, store score
-           
-    # read next line
+    # get headings and ignore
     line = file.readline()
-  
+
+    # read first line  ie. priming loop
+    line = file.readline()
+
+    # process line until empty line read
+    # note: can also use while line
+    while line != "":
+
+        # split line based on comma
+        data = line.split(",")
+        #print( data )
+        
+        # extract and convert data (ie. int and float must be converted)
+        name = data[0].strip()
+        score = int( data[1].strip() )
+        print( f"{name}, {score}" )
+        
+        if name in high_score:
+            # have seen name, so get score and update if higher
+            current_score = high_score[ name ]
+            if score > current_score:
+                high_score[ name ] = score 
+        else:
+            high_score[ name ] = score  # first time see name, store score
+               
+        # read next line
+        line = file.readline()
+        
+        # end loop
+        
 print()
 print( "Done reading file" )
 
